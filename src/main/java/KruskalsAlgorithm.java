9948e3a8659e400a0b2cb62e3d7ed2972eed9062
zhaskanatt <nuadasamuil@gmail.com>
2025-10-26 22:23:29 +0500

    Implement Kruskal’s algorithm to find Minimum Spanning Tree

---
+++ src/main/java/KruskalsAlgorithm.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+import java.util.List;
+
+public class KruskalsAlgorithm {
+
+    public static AlgorithmResult findMST(Graph graph) {
+        int vertices = graph.nodes.size();
+        List<Edge> mstEdges = new ArrayList<>();
+        Collections.sort(graph.edges, Comparator.comparingInt(e -> e.weight));
+
+        UnionFind unionFind = new UnionFind(vertices);
+        long startTime = System.nanoTime();
+
+        for (Edge edge : graph.edges) {
+            int fromIndex = graph.nodes.indexOf(edge.from);
+            int toIndex = graph.nodes.indexOf(edge.to);
+
+            if (unionFind.find(fromIndex) != unionFind.find(toIndex)) {
+                mstEdges.add(edge);
+                unionFind.union(fromIndex, toIndex);
+            }
+        }
+
+        long endTime = System.nanoTime();
+        double executionTimeMs = (endTime - startTime) / 1_000_000.0;
+
+        int totalCost = mstEdges.stream().mapToInt(e -> e.weight).sum();
+        return new AlgorithmResult(mstEdges, totalCost, graph.edges.size(), executionTimeMs);
+    }
+}
+
+class UnionFind {
+    private int[] parent;
+    private int[] rank;
+
+    public UnionFind(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+}
